a9a4105cd74cb6a7a361da4d997642e07c4c57b9
Mykola Manko <mankokolya@gmail.com>
2022-07-09 17:56:09 +0300

    Create CurrencyExchange entity, controller and default values for inmemory database initialization

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -0,0 +1,33 @@
+package com.in28minutes.microservices.currencyexchangeservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class CurrencyExchange {
+    @Id
+    private Long id;
+    @Column(name = "currency_from")
+    private String from;
+    @Column(name = "currency_to")
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String environment;
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+}
