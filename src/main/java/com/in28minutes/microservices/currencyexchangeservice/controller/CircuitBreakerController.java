7331da3b3856b2804f332798043a6b416ed2e7bf
Mykola Manko <mankokolya@gmail.com>
2022-07-17 13:26:09 +0300

    implement rate limiting using resilience4j

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +16,13 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
     public String sampleApi() {
         logger.info("Sample API received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception e) {
